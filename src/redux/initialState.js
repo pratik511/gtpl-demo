948454c2058856eb4b2bcc11be413362f8f0acbb
pratik511 <pp05111999@gmail.com>
2022-05-03 18:38:28 +0530

    4

---
+++ src/redux/initialState.js
@@ -0,0 +1,10 @@
+export let initialState = {
+  Page: 1,
+  Name: "",
+  Contact: "",
+  Package_Type: "",
+  Price: "",
+  Start_Date: "",
+  Package_Plane: "",
+  Total_Price: "",
+};
---
+++ src/redux/type/all.Type.js
@@ -0,0 +1,1 @@
+export const backBtn = "backBtn";
